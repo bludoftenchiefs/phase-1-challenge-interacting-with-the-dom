b8861091391b4452c60892d908dc3544a3ec2ae4
rilith <bludoftenchiefs@protonmail.com>
2021-05-23 01:09:05 -0500

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,39 +1,33 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {    
-    var seconds = 0;
     
-    var el = document.getElementByID('counter');
+    const counter = document.getElementById('counter'); 
+
+    setInterval(function(){
+         counter.innerText++}, 1000);
     
-    function incrementSeconds() {
-        seconds += 1;
-        return incrementSeconds;
-    }
+    const minus = document.getElementById('minus');
+    
+    minus.addEventListener('click', function(){
+        counter.innerText--;
+    })
+
+    const plus = document.getElementById('plus');
+
+    plus.addEventListener('click', function(){
+        counter.innerText++;
+    })
+
+    const heart = document.getElementById('heart');
+
+    heart.addEventListener('click', function(like){
+    
+    })
+
+
 })
 
-
-
-
-// document.querySelector('#counter').addEventListener('counter', function() {
-//     console.log(document.querySelector).timer;
-
-// "use strict";function _toConsumableArray(a){
-//     if(Array.isArray(a)){
-//         for(var b=0,c=Array(a.length);b<a.length;b++)c[b]=a[b];
-//     return c}
-//     return Array.from(a)}var playing=!0,timer=function(){
-//         return setInterval(function(){
-//              var a=document.getElementById("counter"),b=parseInt(a.innerText);a.innerText=b+1},1e3)},interval=timer(),
-            //         minus=document.getElementById("minus"),
-            //         plus=document.getElementById("plus"),
-            //         heart=document.getElementById("heart"),
-            //         pause=document.getElementById("pause"),
-            //         commentForm=document.getElementsByTagName("form")[0];
-//         minus.addEventListener("click",function(){
-//             var a=document.getElementById("counter"),b=parseInt(a.innerText);a.innerText=b-1}),
-//             plus.addEventListener("click",function(){
-//                 var a=document.getElementById("counter"),
-//                 b=parseInt(a.innerText);a.innerText=b+1}),
-//                 heart.addEventListener("click",function(){
+//              heart.addEventListener("click",function(){
 //                     var a=document.getElementById("counter"),
 //                     b=parseInt(a.innerText),c=document.querySelector(".likes"),
 //                     d=void 0;if([].concat(_toConsumableArray(c.children)).map(function(a){
@@ -43,11 +37,13 @@ document.addEventListener('DOMContentLoaded', () => {
 //                         d.innerHTML=b+" has been liked <span>"+(e+1)+"</span> times"}
 //                         else(d=document.createElement("li")).setAttribute("data-num",b),
 //                         d.innerHTML=b+" has been liked <span>1</span> time",
-//                         c.appendChild(d)}),pause.addEventListener("click",function(){
-//                             playing?(playing=!1,clearInterval(interval),this.innerText="resume"):(playing=!0,interval=timer(),
-//                             this.innerText="pause"),
-//                             [].concat(_toConsumableArray(document.getElementsByTagName("button"))).forEach(function(a)
-//                             {"pause"!==a.id&&(a.disabled=!playing)})}),commentForm.addEventListener("submit",function(a)
+//                         c.appendChild(d)}),
+//             pause.addEventListener("click",function(){
+//                          playing?(playing=!1,clearInterval(interval),this.innerText="resume"):(playing=!0,interval=timer(),
+//                           this.innerText="pause"),
+//                           [].concat(_toConsumableArray(document.getElementsByTagName("button"))).forEach(function(a)
+//                             {"pause"!==a.id&&(a.disabled=!playing)})}),
+//             commentForm.addEventListener("submit",function(a)
 //                             {a.preventDefault();
 //                                 var b=this.children[0],c=b.value;b.value="";
 //                                 var d=document.querySelector(".comments"),e=document.createElement("p");e.innerText=c,d.appendChild(e)});
